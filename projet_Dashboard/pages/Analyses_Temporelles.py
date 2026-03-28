be88b2fe8a450bddc4d28df571e49592c21e2752
FredMsv <fredericmsv@gmail.com>
2026-03-28 01:23:33 +0100

    Mise à jour : Intégration de Plotly et optimisation des filtres mois

--- projet_Dashboard/pages/Analyses_Temporelles.py
+++ projet_Dashboard/pages/Analyses_Temporelles.py
@@ -24,7 +24,6 @@ st.sidebar.header("Filtres temporels")
 
 try:
     df_test = regrouper_fichier(fichier_data)
-    df_test["Date"] = pd.to_datetime(df_test["Date"])
 except DossierVideError as e:
     st.error(e)
     st.stop()
@@ -38,17 +37,21 @@ annee_choisie = st.sidebar.multiselect("Annee",
                                        options=sorted(df_test["Date"].dt.year.unique()),
                                        default=df_test["Date"].dt.year.unique())
 
+ 
 dict_mois = {
     1: "Janvier", 2: "Février", 3: "Mars", 4: "Avril", 
     5: "Mai", 6: "Juin", 7: "Juillet", 8: "Août", 
     9: "Septembre", 10: "Octobre", 11: "Novembre", 12: "Décembre"
 }
 
+mois_presents = df_test["Date"].dt.month.unique()
+options_mois = {k:v for k,v in dict_mois.items() if k in mois_presents}
+
 df_test["Mois_Nom"] = df_test["Date"].dt.month.map(dict_mois)
 
 mois_choisi = st.sidebar.multiselect("Mois",
-                                     options=dict_mois.values(),
-                                     default=dict_mois.values())
+                                     options=options_mois.values(),
+                                     default=options_mois.values())
 
 filter_period = (
     (df_test["Region"].isin(region_choisie)) & 
@@ -63,10 +66,18 @@ df_selectionne = df_test[filter_period]
 
 
 if not df_selectionne.empty:
-    st.subheader("Meilleur CA")
+    st.subheader("Meilleur CA sur la période")
     ca_par_region=df_selectionne.groupby("Region")["Ventes"].sum()
     
-    st.metric(label=ca_par_region.idxmax(),value=f"{ca_par_region.max()} $")
+    st.metric(label=ca_par_region.idxmax(),value=f"{ca_par_region.max():,.2f} $")
 
-    ca_par_temps = df_selectionne.groupby("Date")["Ventes"].sum().sort_index()
-    st.line_chart(ca_par_temps)
+    ca_region_temps = df_selectionne.groupby(["Date","Region"])["Ventes"].sum().reset_index()
+    
+    figure = px.line(ca_region_temps,
+                     x="Date",
+                     y="Ventes",color="Region",
+                     title="Evolution CA par periode",
+                     labels={"Ventes": "Chiffre d'Affaires ($)", "Date": "Période"})
+    st.plotly_chart(figure)
+else:
+    st.warning("⚠️ Veuillez sélectionner au moins une région et une période pour afficher les données.")
